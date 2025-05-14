c7822be87ba2d59f0523b4c826e842be490a0051
Akbarzeeshan <akbarzeeshan444@gmail.com>
2025-05-14 12:21:44 +0500

    this is first

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+require('dotenv').config()
+app.get("/" , (req,res)=>{
+res.send("<h1>Hello world<h1/>")
+})
+
+app.listen(process.env.PORT , ()=>{
+    console.log(`the serverin running on ${process.env.PORT}`);
+    
+})
