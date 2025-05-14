73f0ca1ac160997ff5d4259eeaee9c954aecfe1d
Akbarzeeshan <akbarzeeshan444@gmail.com>
2025-05-14 12:22:57 +0500

    this is add another api

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 app.get("/" , (req,res)=>{
 res.send("<h1>Hello world<h1/>")
 })
+app.get("/login" , (req,res)=>{
+res.send("<h1>Login page<h1/>")
+})
 
 app.listen(process.env.PORT , ()=>{
     console.log(`the serverin running on ${process.env.PORT}`);
